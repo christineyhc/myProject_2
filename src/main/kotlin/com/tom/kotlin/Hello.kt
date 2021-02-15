0c704e11c6b5c62bea1a6fc94dcf6a00bcde764c
christine59 <christine59yhc@gmail.com>
2021-02-16 01:33:01 +0800

    Initial commit

---
+++ src/main/kotlin/com/tom/kotlin/Hello.kt
@@ -0,0 +1,12 @@
+package com.tom.kotlin
+
+fun main() {
+    //println("Hello Kotlin")
+    val p= Human();
+    p.hello();
+}
+class Human {
+    fun hello(){
+        println("Hello Kotlin")
+    }
+}
